942a37b4f3fdf3e5eae6fffd676c4e1c9b6de445
Janderson <janderson.pedrosa22@gmail.com>
2025-09-03 00:08:47 -0300

    desafio conpletado, JS montado

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let lista = []
+
+function adicionarAmigo(){
+    let mostrarAmigos = document.querySelector('#listaAmigos');
+    let amigo = document.querySelector('#amigo').value;
+    let input = document.querySelector('#amigo');
+    
+    if(amigo == ''){
+        alert('Digite um nome')
+    }else{
+        mostrarAmigos.innerHTML += `${amigo}<br>`
+        input.value = ''
+        lista.push(amigo)
+    }
+}
+
+function sortearAmigo(){
+    let resultado = document.querySelector('#resultado');
+
+    let sorteioNumero = Math.floor(Math.random() * lista.length);
+
+    resultado.innerHTML = `O amigo secreto sorteado é: ${lista[sorteioNumero]}`
+    
+}
+
+
